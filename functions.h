c42c3c063d3f12f51cae3a698ce30cbbf3884299
Kien Vy <duckien.21@gmail.com>
2021-02-23 12:09:16 +0700

    add a new function to functions.h

--- functions.h
+++ functions.h
@@ -1,6 +1,12 @@
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
-int add (int x, int y);
+/* Performs addition operations. */
+int add (int x, int y); 
+
+/* Performs multiplication operations. */
+int multiply (int x, int y);
+
+
 
 #endif /* FUNCTIONS_H_ */
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include "functios.h"
+#include "functions.h"
+
 int main(void) {
-	printf("hello world");
+	printf("hello world\n");
 	int x = 2;
 	int y = 3;
 	int a = add (2, 3);
